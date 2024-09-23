a56733604fe5b15bf673e7b7e283fc6092806f5b
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-24 00:01:09 +0200

    Add ProfileUnloadEvent and update ProfileLoadEvent
    Introduced ProfileUnloadEvent to handle user profile unloading. Updated ProfileLoadEvent to include an AquaticPlayer parameter, enhancing event handling capabilities.

--- src/main/kotlin/gg/aquatic/waves/profile/event/ProfileUnloadEvent.kt
+++ src/main/kotlin/gg/aquatic/waves/profile/event/ProfileUnloadEvent.kt
@@ -3,8 +3,7 @@ package gg.aquatic.waves.profile.event
 import gg.aquatic.aquaticseries.lib.util.AquaticEvent
 import gg.aquatic.waves.profile.AquaticPlayer
 
-class ProfileLoadEvent(
+class ProfileUnloadEvent(
     val profile: AquaticPlayer
 ): AquaticEvent() {
-
 }
