525a1eda775217b14d7da9e03bee0f193d7549ab
MrLarkyy_ <studiotadikl@gmail.com>
2024-09-23 11:03:06 +0200

    Implement currency initialization in EconomyProfileModule.
    Refactored EconomyHandler to delegate currency registration to EconomyProfileModule. Added SQL logic to initialize and register currencies within EconomyProfileModule, ensuring unique currency IDs. Updated database schema to enforce currency_id uniqueness.

--- src/main/kotlin/gg/aquatic/waves/economy/EconomyHandler.kt
+++ src/main/kotlin/gg/aquatic/waves/economy/EconomyHandler.kt
@@ -3,18 +3,19 @@ package gg.aquatic.waves.economy
 import gg.aquatic.waves.Waves
 import gg.aquatic.waves.module.WaveModule
 import gg.aquatic.waves.module.WaveModules
+import gg.aquatic.waves.profile.module.impl.economy.EconomyProfileModule
+import gg.aquatic.waves.registry.WavesRegistry
 import java.util.concurrent.CompletableFuture
 
 object EconomyHandler: WaveModule {
 
     fun register(currency: CustomCurrency): CompletableFuture<RegisteredCurrency> {
         //WavesRegistry.ECONOMY += currency.id to currency
-
-        val future = CompletableFuture<RegisteredCurrency>()
-
-        // TODO: SQL REGISTRATION
-
-        return future
+        return EconomyProfileModule.initializeEconomy(currency).thenApply {
+            WavesRegistry.INDEX_TO_CURRENCY += it.index to it
+            WavesRegistry.ECONOMY += it.id to it
+            it
+        }
     }
 
     override val type: WaveModules = WaveModules.ECONOMY
--- src/main/kotlin/gg/aquatic/waves/profile/module/impl/economy/EconomyProfileModule.kt
+++ src/main/kotlin/gg/aquatic/waves/profile/module/impl/economy/EconomyProfileModule.kt
@@ -1,13 +1,14 @@
 package gg.aquatic.waves.profile.module.impl.economy
 
+import gg.aquatic.waves.economy.CustomCurrency
+import gg.aquatic.waves.economy.RegisteredCurrency
 import gg.aquatic.waves.profile.AquaticPlayer
 import gg.aquatic.waves.profile.module.ProfileModule
 import gg.aquatic.waves.profile.module.ProfileModuleEntry
 import java.sql.Connection
 import java.util.concurrent.CompletableFuture
 
-class EconomyProfileModule(
-): ProfileModule {
+object EconomyProfileModule: ProfileModule {
     override val id: String = "aquaticeconomy"
 
     val currencyDriver = CurrencyDriver().apply {
@@ -18,11 +19,39 @@ class EconomyProfileModule(
         return currencyDriver.get(aquaticPlayer)
     }
 
+    fun initializeEconomy(currency: CustomCurrency): CompletableFuture<RegisteredCurrency> {
+        val future = CompletableFuture<RegisteredCurrency>()
+        CompletableFuture.runAsync {
+            val rs = currencyDriver.driver.executeQuery("SELECT * FROM aquaticcurrency_type WHERE currency_id = ?") {
+                setString(1, currency.id)
+            }
+
+            if (rs.next()) {
+                val id = rs.getInt("id")
+                val registeredCurrency = RegisteredCurrency(currency, id)
+                future.complete(registeredCurrency)
+                return@runAsync
+            }
+            currencyDriver.driver.useConnection {
+                prepareStatement("INSERT INTO aquaticcurrency_type (currency_id) VALUES (?)").use { preparedStatement ->
+                    preparedStatement.setString(1, currency.id)
+                    preparedStatement.execute()
+                    val rs = preparedStatement.generatedKeys
+                    rs.next()
+                    val id = rs.getInt(1)
+                    val registeredCurrency = RegisteredCurrency(currency, id)
+                    future.complete(registeredCurrency)
+                }
+            }
+        }
+        return future
+    }
+
     override fun initialize(connection: Connection) {
         connection.prepareStatement("CREATE TABLE IF NOT EXISTS " +
                 "aquaticcurrency_type (" +
                 "id INTEGER NOT NULL," +
-                "currency_id NVARCHAR(64) NOT NULL"+
+                "currency_id NVARCHAR(64) NOT NULL UNIQUE"+
                 ")"
         ).use { preparedStatement ->
             preparedStatement.execute()
