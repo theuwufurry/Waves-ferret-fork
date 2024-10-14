6f4bcfd230c0fbd94472b6dabfec73b0c8bc058e
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-14 16:03:46 +0200

    Update NAMESPACE_KEY usage for item registry
    Replaced references to ItemHandler.NAMESPACE_KEY with a new constant NAMESPACE_KEY. This change improves code clarity and makes the namespace key definition centralized within ItemRegistry.

--- src/main/kotlin/gg/aquatic/waves/registry/ItemRegistry.kt
+++ src/main/kotlin/gg/aquatic/waves/registry/ItemRegistry.kt
@@ -1,17 +1,19 @@
 package gg.aquatic.waves.registry
 
+import gg.aquatic.aquaticseries.lib.AquaticSeriesLib
 import gg.aquatic.aquaticseries.lib.item2.AquaticItem
-import gg.aquatic.aquaticseries.lib.item2.ItemHandler
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 
+val NAMESPACE_KEY = NamespacedKey(AquaticSeriesLib.INSTANCE.plugin, "Custom_Item_Registry")
 
 fun AquaticItem.register(namespace: String, id: String): Boolean {
     val registryId = registryId()
     val item = getUnmodifiedItem()
     if (registryId != null) return false
     val meta = item.itemMeta ?: return false
-    meta.persistentDataContainer.set(ItemHandler.NAMESPACE_KEY, PersistentDataType.STRING, "$namespace:$id")
+    meta.persistentDataContainer.set(NAMESPACE_KEY, PersistentDataType.STRING, "$namespace:$id")
     item.itemMeta = meta
     WavesRegistry.ITEM["$namespace:$id"] = this
     return true
@@ -20,7 +22,7 @@ fun AquaticItem.register(namespace: String, id: String): Boolean {
 fun AquaticItem.registryId(): String? {
     val meta = getUnmodifiedItem().itemMeta
     val pdc = meta?.persistentDataContainer ?: return null
-    return pdc.get(ItemHandler.NAMESPACE_KEY, PersistentDataType.STRING)
+    return pdc.get(NAMESPACE_KEY, PersistentDataType.STRING)
 }
 
 fun WavesRegistry.getItem(id: String): AquaticItem? {
@@ -29,7 +31,7 @@ fun WavesRegistry.getItem(id: String): AquaticItem? {
 
 fun WavesRegistry.getItem(itemStack: ItemStack): AquaticItem? {
     val pdc = itemStack.itemMeta?.persistentDataContainer ?: return null
-    val namespacedKey = ItemHandler.NAMESPACE_KEY
+    val namespacedKey = NAMESPACE_KEY
     if (!pdc.has(namespacedKey, PersistentDataType.STRING)) return null
     val id = pdc.get(namespacedKey, PersistentDataType.STRING)
     return ITEM[id]
