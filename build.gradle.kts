51e5d38f3753375f8d703edcb9ee4310c4a7c80a
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-27 02:04:20 +0100

    Update version and dependencies in build.gradle.kts
    Upgraded project version from 1.0.12 to 1.0.15. Updated aquaticlib dependency from version 1.0.31 to 1.0.33, ensuring compatibility and incorporating the latest features.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "gg.aquatic.waves"
-version = "1.0.12"
+version = "1.0.15"
 
 val ktor_version: String by project
 
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-    implementation("gg.aquatic.aquaticseries:aquaticlib:1.0.31:all") {
+    implementation("gg.aquatic.aquaticseries:aquaticlib:1.0.33:all") {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib")
         exclude("gg.aquatic.aquaticseries.core", "core")
         exclude("gg.aquatic.aquaticseries.paper", "paper")
