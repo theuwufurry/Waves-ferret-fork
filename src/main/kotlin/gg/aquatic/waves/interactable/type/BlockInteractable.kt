0f55b8eb2cd911bffd57013d72fa9f7ec1ae2614
MrLarkyy_ <studiotadikl@gmail.com>
2024-11-13 21:57:10 +0100

    Refactor BlockInteractable class to inherit from Interactable.
    Migrated BlockInteractable to extend the Interactable class for consistency. Introduced FakeBlock for encapsulating block interactions and delegated audience, location, and viewer management to FakeBlock.

--- src/main/kotlin/gg/aquatic/waves/interactable/type/BlockInteractable.kt
+++ src/main/kotlin/gg/aquatic/waves/interactable/type/BlockInteractable.kt
@@ -1,4 +1,38 @@
 package gg.aquatic.waves.interactable.type
 
-class BlockInteractable {
+import gg.aquatic.aquaticseries.lib.audience.AquaticAudience
+import gg.aquatic.waves.fake.block.FakeBlock
+import gg.aquatic.waves.interactable.Interactable
+import org.bukkit.Location
+import org.bukkit.entity.Player
+
+class BlockInteractable(
+    val block: FakeBlock
+): Interactable() {
+
+    override var audience: AquaticAudience
+        get() {
+            return block.audience
+        }
+        set(value) {
+            block.audience = value
+        }
+
+    override val location: Location
+        get() {
+            return block.location
+        }
+    override val viewers: MutableSet<Player>
+        get() {
+            return block.viewers
+        }
+
+    override fun addViewer(player: Player) {
+        block.addViewer(player)
+    }
+
+    override fun removeViewer(player: Player) {
+        block.removeViewer(player)
+    }
+
 }
