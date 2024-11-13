adc64ca8934075c8c3f53b87298aa48b19ca201e
MrLarkyy_ <studiotadikl@gmail.com>
2024-11-13 23:43:35 +0100

    Add methods for Player item changes and book opening
    Introduced methods for changing player inventory items and opening books to enhance player interaction capabilities. These changes involve sending packets for item updates and managing book metadata for a seamless user experience.

--- src/main/kotlin/gg/aquatic/waves/util/PlayerExt.kt
+++ src/main/kotlin/gg/aquatic/waves/util/PlayerExt.kt
@@ -1,12 +1,24 @@
 package gg.aquatic.waves.util
 
 import com.github.retrooper.packetevents.PacketEvents
+import com.github.retrooper.packetevents.protocol.player.InteractionHand
 import com.github.retrooper.packetevents.protocol.player.User
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerOpenBook
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetSlot
 import gg.aquatic.waves.entity.EntityHandler
+import io.github.retrooper.packetevents.adventure.serializer.json.JSONComponentSerializer
+import io.github.retrooper.packetevents.util.SpigotConversionUtil
+import io.github.retrooper.packetevents.util.SpigotReflectionUtil
+import io.th0rgal.oraxen.utils.ReflectionUtils
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.md_5.bungee.chat.ComponentSerializer
+import org.bukkit.Material
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.PlayerInventory
+import org.bukkit.inventory.meta.BookMeta
 
 fun Player.toUser(): User {
     return PacketEvents.getAPI().playerManager.getUser(this)
@@ -23,4 +35,25 @@ fun Player.showGlow(entity: Entity, visible: Boolean, color: NamedTextColor? = n
     EntityHandler.modifyEntity(this,entity) {
         setGlow(visible, color)
     }
+}
+
+fun Player.sendItemChange(slot: Int, itemStack: ItemStack) {
+    val packet = WrapperPlayServerSetSlot(0,0,slot,SpigotConversionUtil.fromBukkitItemStack(itemStack))
+    toUser().sendPacket(packet)
+}
+
+fun Player.openBook(pages: List<Component>) {
+    val bookItem = ItemStack(Material.WRITTEN_BOOK)
+    val bookMeta = bookItem.itemMeta as BookMeta
+    val newPages = pages.map {
+        ComponentSerializer.parse(JSONComponentSerializer.json().serialize(it))
+    }
+    bookMeta.spigot().pages = newPages
+    bookItem.itemMeta = bookMeta
+
+    val previousItem = this.inventory.itemInMainHand
+    sendItemChange(this.inventory.heldItemSlot, bookItem)
+    val packet = WrapperPlayServerOpenBook(InteractionHand.MAIN_HAND)
+    toUser().sendPacket(packet)
+    sendItemChange(this.inventory.heldItemSlot, previousItem)
 }
