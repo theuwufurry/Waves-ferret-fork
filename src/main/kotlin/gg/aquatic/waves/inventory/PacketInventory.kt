c45a908d27f4affc942abb6ea1ec1807eca61663
MrLarkyy_ <studiotadikl@gmail.com>
2024-12-08 15:48:37 +0100

    Make PacketInventory class open for extension
    The PacketInventory class changed from final to open, allowing other classes to inherit from it. This update is useful for extending functionality within different contexts or modules without modifying the original class.

--- src/main/kotlin/gg/aquatic/waves/inventory/PacketInventory.kt
+++ src/main/kotlin/gg/aquatic/waves/inventory/PacketInventory.kt
@@ -7,7 +7,7 @@ import org.bukkit.inventory.ItemStack
 import java.util.UUID
 import java.util.concurrent.ConcurrentHashMap
 
-class PacketInventory(
+open class PacketInventory(
     title: Component,
     val type: InventoryType
 ) : Cloneable {
