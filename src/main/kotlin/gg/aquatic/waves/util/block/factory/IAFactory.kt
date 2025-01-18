fdecdd9977399fab773c6beac2c58633a8fe149f
MrLarkyy_ <studiotadikl@gmail.com>
2025-01-18 23:17:22 +0100

    Implement block loading for ItemsAdder and Oraxen factories
    Added `ItemsAdderBlock` and `OraxenBlock` implementations to the respective factories. This enables proper handling of block generation for each block type, replacing placeholder TODOs with functional code.

--- src/main/kotlin/gg/aquatic/waves/util/block/factory/IAFactory.kt
+++ src/main/kotlin/gg/aquatic/waves/util/block/factory/IAFactory.kt
@@ -1,10 +1,11 @@
 package gg.aquatic.waves.util.block.factory
 
 import gg.aquatic.waves.util.block.AquaticBlock
+import gg.aquatic.waves.util.block.impl.ItemsAdderBlock
 import org.bukkit.configuration.ConfigurationSection
 
-object IAFactory: BlockFactory {
+object IAFactory : BlockFactory {
     override fun load(section: ConfigurationSection, material: String): AquaticBlock? {
-        TODO("Not yet implemented")
+        return ItemsAdderBlock(material)
     }
 }
--- src/main/kotlin/gg/aquatic/waves/util/block/factory/OraxenFactory.kt
+++ src/main/kotlin/gg/aquatic/waves/util/block/factory/OraxenFactory.kt
@@ -1,10 +1,12 @@
 package gg.aquatic.waves.util.block.factory
 
 import gg.aquatic.waves.util.block.AquaticBlock
+import gg.aquatic.waves.util.block.impl.OraxenBlock
+import io.th0rgal.oraxen.api.OraxenBlocks
 import org.bukkit.configuration.ConfigurationSection
 
-object OraxenFactory: BlockFactory {
-    override fun load(section: ConfigurationSection, material: String): AquaticBlock? {
-
+object OraxenFactory : BlockFactory {
+    override fun load(section: ConfigurationSection, material: String): AquaticBlock {
+        return OraxenBlock(material)
     }
 }
